2201857e443d14082f8523c6e731fb0096276de6
haavapet <havardp_96@hotmail.no>
2020-05-15 20:34:54 +0200

    changed tab header name

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>See if deployment works</p>
+        <p>Here i will be showcasing my projects</p>
         <a
           className="App-link"
           href="https://reactjs.org"
